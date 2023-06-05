7e584d2dae5464e171b182479b6d36a94b374c0f
masaya <kine0718@icloud.com>
2023-06-05 12:03:29 +0900

    微調整

--- JsonToExcel.py
+++ JsonToExcel.py
@@ -5,16 +5,18 @@ import glob
 # Creating an empty Dataframe with column names only
 jsonData = pd.DataFrame(columns=['fileName','respirationRateMelanin', 'heartRateStddev', 'lfhf','lf','respirationRate','hf','sampleCount','heartRate'])
 
-INPUT_DIRS = 
-INPUT_FILES = glob.glob(INPUT_DIRS+"*\\*.json")
+##Input data
+INPUT_DIRS = ''
+INPUT_FILES = glob.glob(INPUT_DIRS+"*\\vitalsensing*.json")
 INPUT_FILES =sorted(INPUT_FILES)
 
 
-
+##Getting data from files
 for i in range(len(INPUT_FILES)):
     with open(INPUT_FILES[i]) as f:
         data = json.load(f)
         jsonData = jsonData.append({'fileName':f.name,'respirationRateMelanin':data['respirationRateMelanin'],'heartRateStddev':data['heartRateStddev'],'lfhf':data['lfhf'],'lf':data['lf'],'respirationRate':data['respirationRate'],'hf':data['hf'],'sampleCount':data['sampleCount'],'heartRate':data['heartRate']}, ignore_index=True)
         print(f.name)
 
-jsonData.to_excel('sample.xlsx', index=False)
+##exports to an excel file
+jsonData.to_excel('vitalsensing.xlsx', index=False)
